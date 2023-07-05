127f1d9d1aa8a515f9de70a431333a5db9307fda
Mohammad-Ab <root.alpha1@gmail.com>
2023-07-05 23:15:20 +0330

    write special permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
+from .permissions import IsSuperUser,IsAuthorOrReadOnly,IsStaffOrReadOnly,IsSuperUserOrStaffReadOnly
 from blog.models import Article
 from .serializers import ArticleSerializer,UserSerializer
 from django.contrib.auth.models import User
@@ -12,13 +13,14 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permissions_classes = (IsStaffOrReadOnly,IsAuthorOrReadOnly)
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permissions_classes = (IsAdminUser,)
+    permissions_classes = (IsSuperUserOrStaffReadOnly,)
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permissions_classes = (IsAdminUser,)
+    permissions_classes = (IsSuperUserOrStaffReadOnly,)
