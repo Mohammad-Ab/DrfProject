98f0d9b1014860f3c6377d72daa135b383b154d1
Mohammad-Ab <root.alpha1@gmail.com>
2023-07-09 17:56:57 +0330

    filter the objects using simple method

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,8 +12,22 @@ from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
 class ArticleViewSet(ModelViewSet):
-    queryset = Article.objects.all()
+    #queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.all()
+
+        status = self.request.query_params.get('status')
+        if status is not None:
+            queryset = queryset.filter(status=status)
+
+        author = self.request.query_params.get('author')
+        if author is not None:
+            queryset = queryset.filter(author__username=author)
+
+        return queryset
+
     #for permissions is diffrente
     def get_permission(self):
         if self.action in ['list','create']:
